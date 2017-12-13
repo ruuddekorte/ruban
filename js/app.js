3c68b89f06960fc67074682dca53ba4cf0e9b335
Pidgeey <ludwig.somo@gmail.com>
2017-12-13 16:37:47 +0100

    commit simple

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 
-/*  #########   Projet RUBAN   #########   */
+$(document).ready(function(){
+    var clicked = false;
+  $(".ribbon-ciseaux").hover(function(){
+      if($(this).hasClass("zoomed")){
+      $(this).removeClass("zoomed");
+      }else{
+      $(this).addClass("zoomed");
+      }
+    
+  });
+  $(".ribbon-ciseaux").click(function(){
+      if($(this).hasClass("zoomed")){
+        $(this).removeClass("zoomed");
+      }else{
+          $(this).addClass("zoomed");
+        
+      }
+  });
+});
 
 
 
